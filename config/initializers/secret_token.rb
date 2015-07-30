dbdf4a5ad5422e10db9bf8c607a89eb56a763c03
Mauricio <mauricio.pautasso@gmail.com>
2015-07-30 16:36:55 -0300

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Riocuartotrailrun::Application.config.secret_token = '77fc2cbaa71c58cd54fdba6cb50063d59ef19e6ce07060fb078355943d10a34ab27c979adca56492c2dffa313c1e63c9641cc0b46420a7ba84856d05c6e31a2d'
